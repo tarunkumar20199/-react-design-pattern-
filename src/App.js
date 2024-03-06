78ffb52dff74d214e53ad6453f5aa7abf400533f
tarun_kumar <tarunkumar20199>
2024-03-06 18:34:05 +0530

    layout component

--- src/App.js
+++ src/App.js
@@ -1,14 +1,15 @@
-// import { SplitScreen } from "./components/split-screen";
+// --------------------------------------LAYOUT COMPONENT------------------------------
 
-import { Modal } from "./components/Modal";
+// import { SplitScreen } from "./components/split-screen";
+// import { Modal } from "./components/Modal";
 // import { LargeAuthorListItem } from "./components/authors/LargeListItem";
 // import { SmallAuthorListItem } from "./components/authors/SmallListItems";
-import { LargeBookListItem } from "./components/books/LargeListItem";
+// import { LargeBookListItem } from "./components/books/LargeListItem";
 // import { SmallBookListItem } from "./components/books/SmallListItem";
 // import { NormalList } from "./components/lists/Normal";
 // import { NumberedList } from "./components/lists/Numbered";
 // import { authors } from "./data/authors";
-import { books } from "./data/books";
+// import { books } from "./data/books";
 // const LeftSideComp = ({ title }) => {
 //   return <h1 style={{ backgroundColor: "red" }}>{title}</h1>;
 // };
@@ -16,36 +17,87 @@ import { books } from "./data/books";
 //   return <h1 style={{ backgroundColor: "blue" }}>{title}</h1>;
 // };
 
+//--------------------------------------CONTAINER COMPONENT------------------------------
+
+// import { UserLoader } from "./components/user-loader";
+// import { CurrentUserLoader } from "./components/current-user-loader";
+// import { ResourceLoader } from "./components/resource-loader";
+// import { BookInfo } from "./components/book-info";
+// import { DataLoader } from "./components/data-source";
+// import axios from "axios";
+// import { UserInfo } from "./components/user-Info";
+// import { DataSourceWithRender } from "./components/data-source-with-render";
+
+// const getDataFromServer = async (url) => {
+//   const response = await axios.get(url);
+//   return response.data;
+// };
 function App() {
+  // --------------------------------------LAYOUT COMPONENT------------------------------
+  // return (
+  //   // <SplitScreen leftWidth={1} rightWidth={3}>
+  //   //   <LeftSideComp title={"Right"} />
+  //   //   <RightSideComp title={"Left"} />
+  //   // </SplitScreen>
+  //   <>
+  //     {/* <NormalList
+  //       items={authors}
+  //       sourceName={"author"}
+  //       ItemComponent={SmallAuthorListItem}
+  //     />
+  //     <NumberedList
+  //       items={authors}
+  //       sourceName={"author"}
+  //       ItemComponent={LargeAuthorListItem}
+  //     />
+  //     <NormalList
+  //       items={books}
+  //       sourceName={"book"}
+  //       ItemComponent={SmallBookListItem}
+  //     />
+  //     <NumberedList
+  //       items={books}
+  //       sourceName={"book"}
+  //       ItemComponent={LargeBookListItem}
+  //     /> */}
+  //     {/* <Modal>
+  //       <LargeBookListItem book={books[0]} />
+  //     </Modal> */}
+
+  //   </>
+  // );
+
+  //--------------------------------------CONTAINER COMPONENT------------------------------
+  // return (
+  //   <>
+  //     {/* <CurrentUserLoader userId={"3"}>
+  //       <UserInfo />
+  //       <UserLoader />
+  //     </CurrentUserLoader> */}
+  //     {/* <UserLoader userId={"3"}>
+  //       <UserInfo />
+  //     </UserLoader> */}
+  //     {/* <ResourceLoader resourceUrl={"/users/2"} resourceName={"user"}>
+  //       <UserInfo />
+  //     </ResourceLoader>
+  //     <ResourceLoader resourceUrl={"/books/2"} resourceName={"book"}>
+  //       <BookInfo />
+  //     </ResourceLoader> */}
+  //     {/* <DataLoader
+  //       getData={() => getDataFromServer("/users/2")}
+  //       resourceName={"user"}
+  //     >
+  //       <UserInfo />
+  //     </DataLoader> */}
+  //     <DataSourceWithRender
+  //       getData={() => getDataFromServer("/users/2")}
+  //       render={(resource) => <UserInfo user={resource} />}
+  //     />
+  //   </>
+  // );
   return (
-    // <SplitScreen leftWidth={1} rightWidth={3}>
-    //   <LeftSideComp title={"Right"} />
-    //   <RightSideComp title={"Left"} />
-    // </SplitScreen>
     <>
-      {/* <NormalList
-        items={authors}
-        sourceName={"author"}
-        ItemComponent={SmallAuthorListItem}
-      />
-      <NumberedList
-        items={authors}
-        sourceName={"author"}
-        ItemComponent={LargeAuthorListItem}
-      />
-      <NormalList
-        items={books}
-        sourceName={"book"}
-        ItemComponent={SmallBookListItem}
-      />
-      <NumberedList
-        items={books}
-        sourceName={"book"}
-        ItemComponent={LargeBookListItem}
-      /> */}
-      <Modal>
-        <LargeBookListItem book={books[0]} />
-      </Modal>
+      <h1>Start Here</h1>
     </>
   );
 }
